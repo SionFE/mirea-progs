e1a83f658aaba2471d873700704a023593095931
SionFE <savinov@privacynet.net>
2021-09-13 13:49:11 +0300

    Добавлена 1-2

---
+++ problem-sol-1/1-2/1-2.jl
@@ -0,0 +1,27 @@
+function markside(r, b)
+    while isborder(r, b)==false
+        move!(r, b)
+        if ismarker(r)==true
+            return 1
+            break
+        end
+        putmarker!(r)
+    end
+    return 0
+end
+function perimeter(r)
+    steps=0
+    while isborder(r, Sud)==false
+        steps+=1
+        move!(r, Sud)
+    end
+    putmarker!(r)
+    for i=3:7
+        if markside(r, HorizonSide(i%4))==1
+            break
+        end
+    end
+    for i=1:steps
+        move!(r, Nord)
+    end
+end
