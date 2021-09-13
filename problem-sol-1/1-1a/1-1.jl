a177d822cd44d19e08e3e34d6be923181e411afb
SionFE <savinov@privacynet.net>
2021-09-13 12:12:50 +0300

    Переименованы папки

---
+++ problem-sol-1/1-1a/1-1.jl
@@ -0,0 +1,16 @@
+function reverse(side)
+    return HorizonSide((Int(side)+2)%4)
+end
+function cross(r)
+    side=[West, Ost, Nord, Sud]
+    for i in (HorizonSide(s) for s in 0:3)
+        while isborder(r,i)==false
+            move!(r,i)
+            putmarker!(r)
+        end
+        while ismarker(r)==true
+            move!(r,reverse(i))
+        end
+    end
+    putmarker!(r)
+end
