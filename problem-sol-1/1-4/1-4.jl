e8a256e76444cf4213e5570bc3840fe88c9b15a3
SionFE <savinov@privacynet.net>
2021-09-18 09:08:14 +0300

    Добавлена 1-4

---
+++ problem-sol-1/1-4/1-4.jl
@@ -0,0 +1,55 @@
+function moveback(countNord,countEast,r)
+    for i=1:countNord
+        move!(r,Nord)
+    end
+    for i=1:countEast
+        move!(r,Ost)
+    end
+end
+function movetoside(r, s)
+    count=0
+    while isborder(r,s)==false
+        move!(r,s)
+        count+=1
+    end
+    return count
+end
+function movetocorner(r)
+    countSud=movetoside(r, HorizonSide(2))
+    countWest=movetoside(r, HorizonSide(1))
+    return countSud, countWest
+end
+function filltherow(r,s)
+    putmarker!(r)
+    for i=1:s
+        move!(r,Ost)
+        putmarker!(r)
+    end
+    while isborder(r,West)==false
+        move!(r,West)
+    end
+    if isborder(r,Nord)==false
+        move!(r,Nord)
+        putmarker!(r)
+        filltherow(r,s-1)
+    end
+end
+function initsize(r)
+    size=0
+    while isborder(r,Ost)==false
+        size+=1
+        move!(r,Ost)
+        putmarker!(r)
+    end
+    return size
+end
+function fill(r)
+    a,b=movetocorner(r)
+    putmarker!(r)
+    size=initsize(r)
+    movetocorner(r)
+    move!(r,Nord)
+    filltherow(r,size-1)
+    movetocorner(r)
+    moveback(a,b,r)
+end
